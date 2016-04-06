4abb7c60d6b8fc86abb6873486f92b49f24ccdc8
Christopher Bourke <cbourke@cse.unl.edu>
2016-04-05 21:27:38 -0500

    Updated writeup; removed code in DataPersister as it is the activity in part 4

--- src/unl/cse/data/DataPersister.java
+++ src/unl/cse/data/DataPersister.java
@@ -1,6 +1,5 @@
 package unl.cse.data;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -12,23 +11,6 @@ public class DataPersister {
 
 	public static void addAlbum(Album a) {
 
-		EntityManager em = null;
-		
-		try {
-			em = emf.createEntityManager();
 
-			em.getTransaction().begin();
-			em.persist(a);
-			em.getTransaction().commit();
-			
-		} catch (Exception e) {
-			System.err.println("Unable to persist album: "+a);
-			e.printStackTrace();
-			em.getTransaction().rollback();
-		} finally {
-			if (em != null && em.isOpen()) {
-				em.close();
-			}
-		}
 	}
 }
